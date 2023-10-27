d5251d5374ae45227bdbfa65929a014d8a552424
neerea <145057234+neerea@users.noreply.github.com>
2023-10-27 17:05:12 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 """
 Created on Tue Oct  4 15:54:58 2022
 A brief comment just to modify the file
-@author: PedroAnquela
+@author: NereadelMazo
 """
 
 from setuptools import setup, find_packages
@@ -18,7 +18,7 @@ setup(
     version                 = VERSION,
     description             = 'Brief description of your package',
     author                  = AUTHOR,
-    author_email            = 'pedro.anquela@ufv.es',
+    author_email            = '9003629@alumnos.ufv.es',
     license                 = 'MIT',
     python_requires         = '>=3.9.5',
     packages                = find_packages(),
